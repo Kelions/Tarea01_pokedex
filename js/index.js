2663f84750be9f43d773e1a8beed639e85a9d026
ArturoReyes <Arturo.reyes.recabarren@gmail.com>
2021-04-22 17:19:43 -0400

    Texto_Eriquecido

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
 
+tinymce.init({
+    selector: '#descripcion_txt',
+    height: 500,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+    'bold italic backcolor | alignleft aligncenter ' +
+    'alignright alignjustify | bullist numlist outdent indent | ' +
+    'removeformat | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+  
+  
+
+
 document.querySelector("#registrar-btn").addEventListener("click", ()=>{
     let nombre = document.querySelector("#nombre_txt").value;
+    let descripcion = tinymce.get("descripcion_txt").getContent();
+    let legendario = document.querySelector("#legendario-si").checked;
+    let tipo = document.querySelector("#tipo-select").value;
 
-    alert("Hola Mundo " + nombre);
+    console.log("Hola Mundo " + nombre,descripcion,legendario,tipo);
 } );
